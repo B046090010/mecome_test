cba589b3011e903e4c485a98bc99f8f750da46d1
KUO, SHIN-CHI <g0936660561@gmail.com>
2020-08-18 17:57:45 +0800

    tables add select

--- tables.php
+++ tables.php
@@ -1,3 +1,5 @@
+<?php 
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -240,7 +242,7 @@
       <hr class="sidebar-divider d-none d-md-block">
       <!-- search -->
 
-      <button id="send" type="button" class="btn btn-icon-split" >
+      <button id="sendTable" type="button" class="btn btn-icon-split" >
         <span class="icon text-white-50">
           <i class="fas fa-arrow-right"></i>
         </span>
@@ -361,61 +363,7 @@
               </div>
             </li>
 
-            <!-- Nav Item - Messages -->
-            <li class="nav-item dropdown no-arrow mx-1">
-              <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-envelope fa-fw"></i>
-                <!-- Counter - Messages -->
-                <span class="badge badge-danger badge-counter">7</span>
-              </a>
-              <!-- Dropdown - Messages -->
-              <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="messagesDropdown">
-                <h6 class="dropdown-header">
-                  Message Center
-                </h6>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                  <div class="dropdown-list-image mr-3">
-                    <img class="rounded-circle" src="https://source.unsplash.com/fn_BT9fwg_E/60x60" alt="">
-                    <div class="status-indicator bg-success"></div>
-                  </div>
-                  <div class="font-weight-bold">
-                    <div class="text-truncate">Hi there! I am wondering if you can help me with a problem I've been having.</div>
-                    <div class="small text-gray-500">Emily Fowler · 58m</div>
-                  </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                  <div class="dropdown-list-image mr-3">
-                    <img class="rounded-circle" src="https://source.unsplash.com/AU4VPcFN4LE/60x60" alt="">
-                    <div class="status-indicator"></div>
-                  </div>
-                  <div>
-                    <div class="text-truncate">I have the photos that you ordered last month, how would you like them sent to you?</div>
-                    <div class="small text-gray-500">Jae Chun · 1d</div>
-                  </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                  <div class="dropdown-list-image mr-3">
-                    <img class="rounded-circle" src="https://source.unsplash.com/CS2uCrpNzJY/60x60" alt="">
-                    <div class="status-indicator bg-warning"></div>
-                  </div>
-                  <div>
-                    <div class="text-truncate">Last month's report looks great, I am very happy with the progress so far, keep up the good work!</div>
-                    <div class="small text-gray-500">Morgan Alvarez · 2d</div>
-                  </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                  <div class="dropdown-list-image mr-3">
-                    <img class="rounded-circle" src="https://source.unsplash.com/Mv9hjnEUHR4/60x60" alt="">
-                    <div class="status-indicator bg-success"></div>
-                  </div>
-                  <div>
-                    <div class="text-truncate">Am I a good boy? The reason I ask is because someone told me that people say this to all dogs, even if they aren't good...</div>
-                    <div class="small text-gray-500">Chicken the Dog · 2w</div>
-                  </div>
-                </a>
-                <a class="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>
-              </div>
-            </li>
+
 
             <div class="topbar-divider d-none d-sm-block"></div>
 
@@ -423,7 +371,6 @@
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">Valerie Luna</span>
-                <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
@@ -465,10 +412,14 @@
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Group</div>
-                                <select id ="sarea" class="selectpicker form-control" data-live-search="true">
-                                    <option data-tokens="">-</option>
-                                    <option data-tokens="">不分區</option>
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Select</div>
+                                <select id ="select" class="selectpicker form-control" multiple>
+                                    <option data-tokens="">Profit</option>
+                                    <option data-tokens="">Sales Amount</option>
+                                    <option data-tokens="">Quantity</option>
+                                    <option data-tokens="">Percentage(Profit)</option>
+                                    <option data-tokens="">Percentage(Sales)</option>
+                                    <option data-tokens="">Percentage(Quantity)</option>
                                 </select>
                             </div>
                         </div>
@@ -480,10 +431,24 @@
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Sub-issue Group</div>
-                      <select id ="sarea" class="selectpicker form-control" data-live-search="true">
-                        <option data-tokens="">-</option>
-                        <option data-tokens="">不分區</option>
+                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Group</div>
+                      <select id ="group" class="selectpicker form-control">
+                        <optgroup label="Duration">
+                            <option>Year</option>
+                            <option>Month</option>
+                            <option>Day</option>
+                        </optgroup>
+                        <optgroup label="Location">
+                            <option>Area</option>
+                            <option>County</option>
+                            <option>Town</option>
+                        </optgroup>
+                        <optgroup label="Product">
+                            <option>Main Category</option>
+                            <option>Middle Category</option>
+                            <option>Detail Category</option>
+                            <option>Product Name</option>
+                        </optgroup>
                       </select>
                     </div>
                   </div>
@@ -491,8 +456,37 @@
               </div>
             </div>
 
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Order</div>
+                      <select id ="order" class="selectpicker form-control">
+                        
+                      </select>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
 
-
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Limit</div>
+                      <select id ="sarea" class="selectpicker form-control">
+                        <option data-tokens="">First 100 rows</option>
+                        <option data-tokens="">Last 100 rows</option>
+                        <option data-tokens="">All rows</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
          </div>
 
           <!-- DataTales Example -->
