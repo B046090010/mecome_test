3f5e94484060afd5130d24964cf7c39ddaad85de
KUO, SHIN-CHI <g0936660561@gmail.com>
2020-08-19 10:17:23 +0800

    table function

--- db/db_connect.php
+++ db/db_connect.php
@@ -54,26 +54,82 @@ function input($get){
 }
 function Tables($start,$end,$main,$middle,$detail,$area,$country,$town,$select,$group,$order,$limit){
 	global $db;
-	$temp_select=explode(",", $select);
-	$query='SELECT sum("Total_Sales") as s,sum("Total_Profit") as p,sum("Total_Quantity") as q
-	FROM "Sales_Total" as ST inner join 
-		"Dim_Product" as DP on ST."Product Key"=DP."Product Key" inner join
-		"Dim_Store" as DS on ST."Store Key"= DS."Store Key"
+
+	//where&from
+	$from_where=' FROM "Sales_Total" as ST inner join 
+	"Dim_Product" as DP on ST."Product Key"=DP."Product Key" inner join
+	"Dim_Store" as DS on ST."Store Key"= DS."Store Key"
 	Where ST."Sales Date Key" between '.$start.' AND '. $end.' 
-      and DP."Main Category" '.$main.' AND DP."Middle Category" '.$middle.' AND DP."Detail Category" '.$detail.' AND 
-	  DS."Area" '.$area.' AND DS."Country" '.$country.' AND DS."Town" '.$town.
-	  '	GROUP BY "'.$group.'" 
-	  ORDER BY "'.$order.'"
-	   LIMIT '.$limit;
+	and DP."Main Category" '.$main.' AND DP."Middle Category" '.$middle.' AND DP."Detail Category" '.$detail.' AND 
+	DS."Area" '.$area.' AND DS."Country" '.$country.' AND DS."Town" '.$town;
+
+	//group
+	if ($group=="Year")
+	   $group='"Sales Date Key"/10000';
+	elseif($group=="Month")
+	   $group='"Sales Date Key"/100';
+	elseif ($group=="Day")
+	   $group='"Sales Date Key"';
+	elseif ($group!="Product Name")
+	   $group='"'.$group.'"';
+
+	$temp_select='SELECT '.$group.' AS item,';
+
+	if ($group=="Product Name"){
+	   $group='DP."Product Key"';
+	   $temp_select='SELECT "Product Name Ch", ';
+	}
+	
+	//select
+	$select=explode(",", $select);
+
+	foreach($select as $opt){
+	   if ($opt=="Profit")
+		  $opt = 'SUM("Total_Profit") as p,';
+	   else if ($opt == "Sales Amount")
+		  $opt = 'SUM("Total_Sales") as s,';
+	   else if ($opt == "Quantity")
+		  $opt = 'SUM("Total_Quantity") as q,';
+	   else{
+		  //$opt=preg_split('/\(|\)/', $value)[1];
+		  $opt ='(SUM("Total_Profit") / SUM(SUM("Total_Profit")) OVER ()) AS "Percentage",';
+		  // echo $opt;
+		  // unset($opt);
+	   }
+	   $temp_select=$temp_select.$opt;
+	} 
+	$temp_select= substr($temp_select, 0, -1);
+
+
+	//order
+	if ($order == "Profit")
+	   $order='p';
+	elseif ($order == "Sales Amount")
+	   $order='s';
+	else
+	   $order='q';
+	//limit
+	if ($limit == "First 100 rows")
+	   $limit=' DESC LIMIT 100';
+	elseif ($limit == "Last 100 rows"){
+	   $limit=' ASC LIMIT 100';
+	}
+	else
+	   $limit=' DESC';
+
+	//query
+	$query=$temp_select.$from_where.' GROUP BY '.$group.' ORDER BY '.$order.$limit;
+	//result
 	if($result = pg_query($db, $query)){
-		$rows = pg_fetch_all($result);
-		return $rows;
+	   $data= pg_fetch_all($result);
+	   return $data;
 	}
 	else{
-		echo pg_last_error($db);
-		exit;
+	   echo pg_last_error($db);
+	   exit;
 	}
-}
+	
+ }
 //'SELECT COUNT(*) FROM (SELECT DISTINCT LEFT("_SourceSales",25) FROM "Fact_Sales" WHERE "Date" Between $1 AND $2) AS temp'
 
 //pg_close($db);
