f47d95683df307cae882683b42f2c096935dbbd8
KUO, SHIN-CHI <g0936660561@gmail.com>
2020-08-25 18:34:33 +0800

    model try

--- ml/train.php
+++ ml/train.php
@@ -3,27 +3,32 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 include("../db/db_connect.php");
-ini_set("display_errors","On");
+//ini_set("display_errors","On");
 
 
 
 
-use Phpml\Regression\LeastSquares;
+use Phpml\Regression\SVR;
+use Phpml\SupportVectorMachine\Kernel;
 use Phpml\ModelManager; 
+use Phpml\Association\Apriori;
+
 
 
 function show($arr){
     foreach($arr AS $record){
         foreach ($record AS $key=>$value){
-            echo $key.'=>'.$value.'<p>';
+            echo $key.'=>'.$value.'; Type: '.gettype($value).'<p>';
         }
         echo '================<p>';
     }
 }
 
 $iv=array();
-$dv=array();
-$query='SELECT "Total_Sales","Total_Profit","Total_Cost"
+$dv=[];
+
+
+$query='SELECT "Weekday","Month","Year","Is Weekend","Is Holiday","Main Category","Country","Total_Sales"
 From (
 	"Sales_Total" as ST left join 
 	"Dim_Date" as DD on  ST."Sales Date Key"=DD."Date Key" left join
@@ -41,23 +46,29 @@ else{
 }
 foreach($rows AS $record){
     $temp=[];
-    $temp2=[];
     foreach ($record AS $key=>$value){
-        if($key != "Total_Profit"){
-            array_push($temp,$value);
-        }
-        else{
-            array_push($temp2,$value);
-        }
+        array_push($temp,$value);
     }
     array_push($iv,$temp);
-    array_push($dv,$temp2);
+    //array_push($dv,$temp2);
+
 }
-$regression = new LeastSquares();
-$regression->train($iv, $dv);
-$filepath = './model/temp';
-$modelManager = new ModelManager();
-$modelManager->saveToFile($regression, $filepath);
+show($iv);
+// $samples = [[60], [61], [62], [63], [65]];
+// $targets = [3.1, 3.6, 3.8, 4, 4.1];
+$associator = new Apriori($support = 0.5, $confidence = 0.5);
+$associator->train($iv, $dv);
+print_r($associator->getRules());
+print_r($associator->predict(['1','12','2019','f','f',"藥品","台北市"]));
+
+
+
+// $regression = new SVR(Kernel::LINEAR);
+// $regression->train($iv, $dv);
+
+//save
+// $modelManager = new ModelManager();
+// $modelManager->saveToFile($regression, $filepath);
 
 
 
