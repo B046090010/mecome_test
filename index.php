4b043a5e5bb34fec82997c397b89d199085690b5
KUO, SHIN-CHI <g0936660561@gmail.com>
2020-08-28 23:29:35 +0800

    fix index chartjs error

--- index.php
+++ index.php
@@ -667,6 +667,7 @@
   <script src="js/sb-admin-2.min.js"></script>
 
   <!-- Page level plugins -->
+  <script src="vendor1/chart.js/Chart.min.js"></script>
 
   <!-- Page level custom scripts -->
   <script src="js/demo/chart-pie-demo.js"></script>
