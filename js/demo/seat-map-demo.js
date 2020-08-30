afb35c13a5ab52ffabd7a50dd060e6d37d261526
KUO, SHIN-CHI <g0936660561@gmail.com>
2020-08-30 13:38:20 +0800

    seatmap

--- js/demo/seat-map-demo.js
+++ js/demo/seat-map-demo.js
@@ -15,7 +15,7 @@ anychart.onDocumentReady(function () {
     //     'seat-map/sport-mall.svg"' +
     //     'target="_blank">SVG Image</a></p></div>'
     // );
-    
+
     // set svg file
     $.ajax({
         type: 'GET',
@@ -23,114 +23,114 @@ anychart.onDocumentReady(function () {
         // The data that have been used for this sample can be taken from the CDN
         // load SVG image using jQuery ajax
         success: function (svgData) {
-        // data for creating a SeatMap
-        var chart = anychart.seatMap([
-            { id: 'nike', value: 'Nike' },
-            { id: 'adidas', value: 'Adidas' },
-            { id: 'puma', value: 'Puma' },
-            { id: 'reebok', value: 'Reebok' }
-        ]);
-        // set svg data
-        chart.geoData(svgData);
-        // load svg-file how it looked(colors stroke/fill)
-        chart
-            .unboundRegions('as-is')
-            // set chart padding
-            .padding([10]);
+            // data for creating a SeatMap
+            var chart = anychart.seatMap([
+                { id: 'nike', value: 'Nike' },
+                { id: 'adidas', value: 'Adidas' },
+                { id: 'puma', value: 'Puma' },
+                { id: 'reebok', value: 'Reebok' }
+            ]);
+            // set svg data
+            chart.geoData(svgData);
+            // load svg-file how it looked(colors stroke/fill)
+            chart
+                .unboundRegions('as-is')
+                // set chart padding
+                .padding([10]);
 
-        // create chart legend
-        var legend = chart.legend();
-        legend
-            .enabled(true)
-            // items source mode categories
-            .itemsSourceMode('categories')
-            .position('right')
-            .itemsLayout('vertical');
+            // create chart legend
+            var legend = chart.legend();
+            legend
+                .enabled(true)
+                // items source mode categories
+                .itemsSourceMode('categories')
+                .position('right')
+                .itemsLayout('vertical');
 
-        var series = chart.getSeries(0);
-        // set color scale.
-        series.colorScale(
-            anychart.scales.ordinalColor([
-            { equal: 'Nike', color: 'rgb(127, 210, 235)' },
-            { equal: 'Adidas', color: 'rgb(111, 193, 117)' },
-            { equal: 'Puma', color: 'rgb(242, 203, 117)' },
-            { equal: 'Reebok', color: 'rgb(188, 139, 191)' }
-            ])
-        );
-
-        // sets stroke/fill series
-        series.stroke(returnColor);
-        series.fill(returnColor);
-
-        // sets fill on hover series and select series
-        series.hovered().fill(returnColorHoverAndSelect);
-        series.selected().fill(returnColorHoverAndSelect);
-
-        // Create chart tooltip own title
-        series.tooltip().title().useHtml(true);
-        series.tooltip().titleFormat(function () {
-            var openTime = {};
-
-            switch (this.regionProperties.id) {
-            case 'nike':
-                openTime = {
-                workingTime: '9AM-8PM',
-                workingTime24Format: '9-20'
-                };
-                break;
-            case 'adidas':
-                openTime = {
-                workingTime: '12AM-9PM',
-                workingTime24Format: '12-21'
-                };
-                break;
-            case 'puma':
-                openTime = {
-                workingTime: '10AM-9PM',
-                workingTime24Format: '10-21'
-                };
-                break;
-            case 'reebok':
-                openTime = {
-                workingTime: '8AM-4PM',
-                workingTime24Format: '8-16'
-                };
-                break;
-            default:
-            }
-
-            var state = isOpen(openTime.workingTime24Format);
-
-            return (
-            this.value +
-            ' - ' +
-            state +
-            '<br><span style="font-size: 10px;">' +
-            openTime.workingTime +
-            '</span>'
+            var series = chart.getSeries(0);
+            // set color scale.
+            series.colorScale(
+                anychart.scales.ordinalColor([
+                { equal: 'Nike', color: 'rgb(127, 210, 235)' },
+                { equal: 'Adidas', color: 'rgb(111, 193, 117)' },
+                { equal: 'Puma', color: 'rgb(242, 203, 117)' },
+                { equal: 'Reebok', color: 'rgb(188, 139, 191)' }
+                ])
             );
-        });
-        // Create chart tooltip own text
-        series.tooltip().format(function () {
-            var textCompany = aboutCompany(); 
 
-            switch (this.regionProperties.id) {
-            case 'nike':
-                return textCompany.nike;
-            case 'adidas':
-                return textCompany.adidas;
-            case 'puma':
-                return textCompany.puma;
-            case 'reebok':
-                return textCompany.reebok;
-            default:
-            }
-        });
+            // sets stroke/fill series
+            series.stroke(returnColor);
+            series.fill(returnColor);
 
-        // set container id for the chart
-        chart.container(stage);
-        // initiate chart drawing
-        chart.draw();
+            // sets fill on hover series and select series
+            series.hovered().fill(returnColorHoverAndSelect);
+            series.selected().fill(returnColorHoverAndSelect);
+
+            // Create chart tooltip own title
+            series.tooltip().title().useHtml(true);
+            series.tooltip().titleFormat(function () {
+                var openTime = {};
+
+                switch (this.regionProperties.id) {
+                case 'nike':
+                    openTime = {
+                    workingTime: '9AM-8PM',
+                    workingTime24Format: '9-20'
+                    };
+                    break;
+                case 'adidas':
+                    openTime = {
+                    workingTime: '12AM-9PM',
+                    workingTime24Format: '12-21'
+                    };
+                    break;
+                case 'puma':
+                    openTime = {
+                    workingTime: '10AM-9PM',
+                    workingTime24Format: '10-21'
+                    };
+                    break;
+                case 'reebok':
+                    openTime = {
+                    workingTime: '8AM-4PM',
+                    workingTime24Format: '8-16'
+                    };
+                    break;
+                default:
+                }
+
+                var state = isOpen(openTime.workingTime24Format);
+
+                return (
+                this.value +
+                ' - ' +
+                state +
+                '<br><span style="font-size: 10px;">' +
+                openTime.workingTime +
+                '</span>'
+                );
+            });
+            // Create chart tooltip own text
+            series.tooltip().format(function () {
+                var textCompany = aboutCompany(); 
+
+                switch (this.regionProperties.id) {
+                case 'nike':
+                    return textCompany.nike;
+                case 'adidas':
+                    return textCompany.adidas;
+                case 'puma':
+                    return textCompany.puma;
+                case 'reebok':
+                    return textCompany.reebok;
+                default:
+                }
+            });
+
+            // set container id for the chart
+            chart.container(stage);
+            // initiate chart drawing
+            chart.draw();
         }
     });
 });
